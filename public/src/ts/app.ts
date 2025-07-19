e13b9d7b1217461d7e8815706308d08e2e1066d6
Harshit Sah <harshitsah96@gmail.com>
2025-07-19 17:16:19 +0530

    minor update 1.1

--- public/src/ts/app.ts
+++ public/src/ts/app.ts
@@ -201,6 +201,7 @@ export function initAuth(onAuthSuccess: () => void): void {
           method: "POST",
           body: { email, password },
         });
+        console.log(data.accessToken)
         localStorage.setItem("accesstoken", data.accessToken);
         localStorage.setItem("user", JSON.stringify(data.user));
         document.body.className = "";
