8a7599a4607bcf6d5deb6112a0645c718f1a05b9
sahashiharshit <harshitsah96@gmail.com>
2025-08-13 19:30:06 +0530

    removed hot reload

--- public/src/ts/hmr.ts
+++ public/src/ts/hmr.ts
@@ -1,24 +1,24 @@
-let currentModule:any;
-let currentPath = "./app.js";
+// let currentModule:any;
+// let currentPath = "./app.js";
 
-async function load(modulePath:string) {
-    if(currentModule?.dispose) currentModule.dispose();
+// async function load(modulePath:string) {
+//     if(currentModule?.dispose) currentModule.dispose();
     
-    const mod = await import(modulePath + `?hot=${Date.now()}`);
-    currentModule = mod;
-    currentPath=modulePath;
-    mod.init();
-    }
+//     const mod = await import(modulePath + `?hot=${Date.now()}`);
+//     currentModule = mod;
+//     currentPath=modulePath;
+//     mod.init();
+//     }
 
-load(currentPath);
+// load(currentPath);
 
-const socket = new WebSocket("ws://localhost:35729");
+// const socket = new WebSocket("ws://localhost:35729");
 
-socket.onmessage=(e)=>{
+// socket.onmessage=(e)=>{
 
-    if(e.data==="reload"){
-        console.log("HMR: Reload signal received",currentPath);
-        load(currentPath);
-    }
-};
-(window as any).hmrLoad = load;
+//     if(e.data==="reload"){
+//         console.log("HMR: Reload signal received",currentPath);
+//         load(currentPath);
+//     }
+// };
+// (window as any).hmrLoad = load;
